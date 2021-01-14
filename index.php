7bd16503e0ffbd2f81fa72d50604f6f4eecdb0cc
Code-Schmankerl <77410029+Code-Schmankerl@users.noreply.github.com>
2021-01-14 10:19:21 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <!--<link rel="stylesheet" href="style.css">-->
+    <title></title>
+  </head>
+  <body>
+    <?php 
+        echo $_SERVER['DOCUMENT_ROOT'];
+  </body>
+</html>
